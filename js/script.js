4eb4d521fa3abd4cddfe276a6e8703e2daa70d2f
Viacheslav <mibdeveloper@gmail.com>
2015-10-08 11:51:03 +0300

    next prev controls

--- js/script.js
+++ js/script.js
@@ -3,6 +3,37 @@ $(function() {
         // Configuration goes here
     });
 
-    $('.jcarousel').jcarousel('scroll', '+=2');
-});
+    //$('.jcarousel').jcarousel('scroll', '+=2');
+/*
+    $('.jcarousel-control-prev').click(function() {
+        $('.jcarousel').jcarousel('scroll', '-=1');
+    });
 
+    $('.jcarousel-control-next').click(function() {
+        $('.jcarousel').jcarousel('scroll', '+=1');
+    });
+		*/
+			$('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+
+});
